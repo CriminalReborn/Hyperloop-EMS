4288eb528b24f9b1ce3ce23fcf93a1ff6a2d5d77
zawata <zawataza@gmail.com>
2016-01-15 16:30:33 -0700

    update Node Modules

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-var net = require('net');
+const net = require('net');
 
-var HOST = '127.0.0.1';
-var PORT = 6969;
+var address
 
-// Create a server instance, and chain the listen function to it
-// The function passed to net.createServer() becomes the event handler for the 'connection' event
-// The sock object the callback function receives UNIQUE for each connection
-net.createServer(function(sock) {
-    
-    // We have a connection - a socket object is assigned to the connection automatically
-    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-    
-    // Add a 'data' event handler to this instance of socket
+var server = net.createServer(function (sock) {
+	sock.on('connect', function(data) {
+		//connection from client
+		console.log('Connection: ' + sock.remoteAddress);
+	});
     sock.on('data', function(data) {
-        
-        console.log('DATA ' + sock.remoteAddress + ': ' + data);
-        // Write the data back to the socket, the client will receive it as data from the server
-        sock.write('You said "' + data + '"');
-        
+		//receive from client
+		
+        console.log('Data: ' + sock.remoteAddress + " " + data);
     });
-    
-    // Add a 'close' event handler to this instance of socket
+    sock.on('end', function(data) {
+		//client disconnect
+		console.log('Disconnection: ' + sock.remoteAddress);
+	});
     sock.on('close', function(data) {
-        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+		//client shutdown server
+        console.log('Close: ' + sock.remoteAddress);
     });
     
-}).listen(PORT, HOST);
+})
 
-console.log('Server listening on ' + HOST +':'+ PORT);
+server.listen(() => {
+  address = server.address();
+  console.log('opened server on port %j', address.port);
+});
+
+server.write("Test")
